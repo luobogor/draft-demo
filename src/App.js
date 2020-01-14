fdfe490d16cea5a70f3a6568bc081bb05a348c1b
叶锦湛 <jinzhan.ye@foxmail.com>
2020-01-14 21:40:38 +0800

    keyboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {Editor, EditorState} from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 
 class MyEditor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {editorState: EditorState.createEmpty()};
     this.onChange = (editorState) => {
-      console.log('editorState:', editorState)
       return this.setState({editorState})
     };
     this.setEditor = (editor) => {
@@ -23,12 +22,30 @@ class MyEditor extends React.Component {
     this.focusEditor();
   }
 
+  _onBoldClick() {
+    // ?? 不起作用
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+
+  handleKeyCommand(command, editorState) {
+    // RichUtils has information about the core key commands available to web editors,
+    // such as Cmd+B (bold), Cmd+I (italic), and so on.
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
   render() {
     return (
       <div style={styles.editor} onClick={this.focusEditor}>
+        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
         <Editor
           ref={this.setEditor}
           editorState={this.state.editorState}
+          handleKeyCommand={this.handleKeyCommand}
           onChange={this.onChange}
         />
       </div>
